2b0162b504e60c47f4827df9b25aa96a6dbef05b
Xsj <2427970264@qq.com>
2022-06-03 15:59:14 +0800

    [测试提交]

---
+++ RegistrationCenter/src/main/java/com/xiong/test/Test.java
@@ -0,0 +1,18 @@
+package com.xiong.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 测试类
+ * Xsj 2022/6/3 15:52
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Map map = new HashMap<String,String>();
+        map.put("","");
+    }
+
+
+}
