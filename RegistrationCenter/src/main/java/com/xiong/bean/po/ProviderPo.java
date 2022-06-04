813c7c7621bc4a56018c651e6ce8c35e8bc788b3
Xsj <2427970264@qq.com>
2022-06-04 19:09:54 +0800

    [增加bean]

---
+++ RegistrationCenter/src/main/java/com/xiong/bean/po/ProviderPo.java
@@ -0,0 +1,23 @@
+package com.xiong.bean.po;
+
+import java.io.Serializable;
+
+/**
+ * @description: 服务提供者pojo
+ * @author: Xsj
+ * @create: 2022-06-04 19:05
+ **/
+public class ProviderPo implements Serializable {
+
+
+
+
+
+
+
+
+
+
+
+
+}
